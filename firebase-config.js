48d7a31bc9394679df309fea886e6ea34ef242e5
Austin Levi Makachola <77272924+austinlevi@users.noreply.github.com>
2025-07-27 08:59:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,19 @@
 // firebase-config.js
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCA6tCgXGwjJKJ8ewBwrQdWJMzw4TBlQRs",
   authDomain: "nexa-auth-e9e2e.firebaseapp.com",
   projectId: "nexa-auth-e9e2e",
-  storageBucket: "nexa-auth-e9e2e.firebasestorage.app",
+  storageBucket: "nexa-auth-e9e2e.appspot.com",
   messagingSenderId: "470482843400",
   appId: "1:470482843400:web:4ecae871788fd9b7a81ebd",
   measurementId: "G-SRGZFPMJGR"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase and Auth
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
+export { auth };
